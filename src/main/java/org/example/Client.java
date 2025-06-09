32b73802234b525fda19a118d6ee25e8277edb2b
Wojciech <wenter2005pl@gmail.com>
2025-06-09 14:51:08 +0200

    idk

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Client {
+    private String login;
+
+    public String getLogin() {
+        return login;
+    }
+    public void sendMessage(String message){
+
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Server {
+   // private List<Client>clients;
+    private ServerSocket serverSocket;
+    private Map<String, Client> clientMap = new HashMap<>();
+
+
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: 1234");
+        }
+    }
+    public void clientLoggedIn(Client client){
+        clientMap.put(client.getLogin(),client);
+                broadcast(String.format("%s się zalogował", client.getLogin()));
+
+        }
+
+
+    public void clientLoggedOut(Client client){
+        clientMap.remove(client.getLogin());
+        broadcast(String.format("%s się zalogował", client.getLogin()));
+
+    }
+    public void broadcast(String message){
+        for(Client c : clientMap.values()){
+                c.sendMessage(message);
+        }
+    }
+}
