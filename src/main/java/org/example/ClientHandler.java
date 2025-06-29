bd685b0e7a4f1cc00efae4ad93ee078d174916d2
Wojciech <wenter2005pl@gmail.com>
2025-06-29 22:47:21 +0200

    idk

---
+++ src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private Server server;
+    private String login;
+    private Socket socket;
+
+    public ClientHandler(Socket socket,Server server) throws IOException {
+        InputStream input = socket.getInputStream();
+        reader = new BufferedReader(new InputStreamReader(input));
+        OutputStream output = socket.getOutputStream();
+        writer = new PrintWriter(new OutputStreamWriter(output),true);
+        this.server = server;
+        this.socket = socket;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    @Override
+    public void run() {
+        String message;
+        try {
+            login = reader.readLine();
+            server.onClientConnected(this);
+            while((message = reader.readLine()) != null) {
+                System.out.println(message);
+                if(message.equals("/online")) {
+
+                    server.sendOnlineList(this);
+                } else if (message.startsWith("/w ")) {
+                    String[] parts = message.substring("/w ".length()).split(" ", 2);
+                    server.whisper(this, parts[0], parts[1]);
+                } else {
+                    server.broadcast(message,login);
+                }
+
+            }
+            socket.close();
+            server.onClientDisconnected(this);
+        } catch (IOException e) {
+            System.out.println("Client disconnected");
+        }
+//aaadwadwad
+    }
+
+    public void send(String message) {
+        writer.println(message);
+    }
+}
